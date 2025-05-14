6b0df72a10fe2aa9c698e0ef84fc2d38352c607b
Olya Khromova <olya.hrmvlg@gmail.com>
2025-05-15 02:12:40 +0300

    Add layout and styling for App component with global styles

--- ji-test/src/App.jsx
+++ ji-test/src/App.jsx
@@ -1,7 +1,9 @@
+import { Container } from "react-bootstrap";
 
 export default function App() {
     return (
-        <>
-        </>
+        <Container fluid className="wrapper d-flex justify-content-center">
+
+        </Container>
     );
 };
