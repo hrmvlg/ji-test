55234b6ba04a683b0157cbc2cd3b4ec23abce293
Olya Khromova <olya.hrmvlg@gmail.com>
2025-05-15 01:43:05 +0300

    Add CustomButton component and associated styles

---
+++ ji-test/src/components/Button/CustomButton.jsx
@@ -0,0 +1,7 @@
+import { Button } from 'react-bootstrap'
+
+export default function CustomButton() {
+    return (
+        <Button className="button d-flex align-items-center">Записаться онлайн</Button>
+    )
+}
