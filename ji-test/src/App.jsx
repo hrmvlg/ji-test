f8a90f56034aade9979e159ef97963bf991e0710
Olya Khromova <olya.hrmvlg@gmail.com>
2025-05-15 03:50:37 +0300

    Add Information component with styling and integrate into App component

--- ji-test/src/App.jsx
+++ ji-test/src/App.jsx
@@ -1,10 +1,12 @@
 import { Container } from "react-bootstrap";
 import CustomCarousel from './components/Carousel/CustomCarousel';
+import Information from "./components/Information/Information";
 
 export default function App() {
     return (
         <Container fluid className="wrapper d-flex justify-content-center">
             <CustomCarousel />
+            <Information />
         </Container>
     );
 };
---
+++ ji-test/src/components/Information/Information.jsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+
+export default function Information() {
+    return (
+        <Container fluid className="information d-flex flex-column m-0 p-0">
+            <h1 className="information__title h1 p-0">Почему выбирают именно нас?</h1>
+            <p className="information__description ">
+                Мы не просто создаём иллюзию работы, а действительно выполняем её, гарантируя безупречный результат и предоставляя гарантии.
+                <br />
+                <br />
+                После завершения всех процедур в нашем детейлинг-центре, ваш автомобиль будет выглядеть как новый.
+            </p>
+        </Container>
+    );
+};
